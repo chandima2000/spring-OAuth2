d6ecb57a3b58d7bf97b35df06e4e09e91570906f
chandima2000 <hychandima2000@gmail.com>
2024-10-29 22:16:16 +0530

    spring OAuth2 project starter code

---
+++ src/main/java/org/chandima/springoauth/HelloController.java
@@ -0,0 +1,14 @@
+package org.chandima.springoauth;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String greet(){
+        return "Hello World";
+    }
+
+}
